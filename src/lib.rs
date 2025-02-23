a2c40080578633ffc628d9bd7616d254e8a91ab1
Sasquire <onemillian@protonmail.com>
2025-02-23 13:44:53 -0500

    Add local testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+// Used to build for web release
 // cargo bundle-licenses --prefer MIT --format toml --output ./pkg/THIRD_PARTY_LICENSES.toml
 // wasm-pack build --release --no-pack --no-typescript --target web
 
@@ -26,7 +27,7 @@ macro_rules! apply_attrib {
 // https://doc.rust-lang.org/nightly/std/macro.concat_idents.html
 // Waiting on concat_idents to be stable to make this cleaner
 macro_rules! generate_bindings {
-	{ $day:ident, $part_a:ident, $part_b:ident } => {
+	{ $day:ident, $tests:ident, $part_a:ident, $part_b:ident } => {
 		mod $day { pub mod $day; }
 
 		apply_attrib! {
@@ -35,12 +36,37 @@ macro_rules! generate_bindings {
 			pub fn $part_a (input: &str) -> String { $day::$day::$part_a(input) }
 			pub fn $part_b (input: &str) -> String { $day::$day::$part_b(input) }
 		}
+
+		// Test with something like 
+		// cargo test day_06 -- --show-output
+		#[cfg(test)]
+		mod $tests {
+			fn setup_test () -> String {
+				let file_name = format!("./inputs/{}.txt", stringify!($day));
+				let contents = std::fs::read_to_string(&file_name).expect("Input file is not present");
+				return contents;
+			}
+
+			fn interpret_result (part: usize, answer: String) {
+				println!("\t{} part {} returned {}!", stringify!($day), part, answer);
+			}
+
+			#[test]
+			fn part_1_test () {
+				interpret_result(1, crate::$part_a(&setup_test()))
+			}
+		
+			#[test]
+			fn part_2_test () {
+				interpret_result(2, crate::$part_b(&setup_test()))
+			}
+		}
 	}
 }
 
-generate_bindings!(day_01, day_01_1, day_01_2);
-generate_bindings!(day_02, day_02_1, day_02_2);
-generate_bindings!(day_03, day_03_1, day_03_2);
-generate_bindings!(day_04, day_04_1, day_04_2);
-generate_bindings!(day_05, day_05_1, day_05_2);
-generate_bindings!(day_06, day_06_1, day_06_2);
+generate_bindings!(day_01, day_01_tests, day_01_1, day_01_2);
+generate_bindings!(day_02, day_02_tests, day_02_1, day_02_2);
+generate_bindings!(day_03, day_03_tests, day_03_1, day_03_2);
+generate_bindings!(day_04, day_04_tests, day_04_1, day_04_2);
+generate_bindings!(day_05, day_05_tests, day_05_1, day_05_2);
+generate_bindings!(day_06, day_06_tests, day_06_1, day_06_2);
